3c6e87ba71340f33150b62200f84228b3df27127
RamikLPNU <roman.nestor.ir.2023@lpnu.ua>
2023-09-28 07:50:57 +0300

    Покращив читабельність

--- module/find_unsorted_subarray.py
+++ module/find_unsorted_subarray.py
@@ -1,15 +1,16 @@
 class Solution:
     @staticmethod
     def find_unsorted_subarray(nums):
-        left = 0 
-        right = len(nums) - 1
+        n = len(nums) - 1
 
-        while left < len(nums) - 1 and nums[left] <= nums[left + 1]:
+        left = 0
+        while left < n and nums[left] <= nums[left + 1]:
             left += 1
 
-        if left == len(nums) - 1:
+        if left == n:
             return (-1, -1)
 
+        right = n
         while right > 0 and nums[right] >= nums[right - 1]:
             right -= 1
 
@@ -19,7 +20,7 @@ class Solution:
         while left >= 0 and nums[left] > subarray_min:
             left -= 1
 
-        while right < len(nums) and nums[right] < subarray_max:
+        while right < n and nums[right] < subarray_max:
             right += 1
 
         return (left + 1, right - 1)
