72750d513b3d312d0e0996d4f2e829f9d3d82087
RamikLPNU <roman.nestor.ir.2023@lpnu.ua>
2023-09-28 07:47:16 +0300

    Виправив помилки

--- module/find_unsorted_subarray.py
+++ module/find_unsorted_subarray.py
@@ -1,16 +1,25 @@
-class FindUnsorted:
+class Solution:
     @staticmethod
     def find_unsorted_subarray(nums):
-    
-        left = 0
+        left = 0 
+        right = len(nums) - 1
+
         while left < len(nums) - 1 and nums[left] <= nums[left + 1]:
             left += 1
 
         if left == len(nums) - 1:
             return (-1, -1)
 
-        right = len(nums) - 1
         while right > 0 and nums[right] >= nums[right - 1]:
             right -= 1
 
-        return (left, right)
+        subarray_min = min(nums[left:right + 1])
+        subarray_max = max(nums[left:right + 1])
+
+        while left >= 0 and nums[left] > subarray_min:
+            left -= 1
+
+        while right < len(nums) and nums[right] < subarray_max:
+            right += 1
+
+        return (left + 1, right - 1)
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,28 +1,27 @@
 import unittest
-from module.find_unsorted_subarray import FindUnsorted
+from module.find_unsorted_subarray import Solution
 
-class TestFindUnsorted(unittest.TestCase):
+class TestSolution(unittest.TestCase):
     def test_find_unsorted_subarray(self):
         # Вхідний масив вже відсортований.
         input_array = [1, 2, 3, 4, 5]
-        result = FindUnsorted.find_unsorted_subarray(input_array)
+        result = Solution.find_unsorted_subarray(input_array)
         self.assertEqual(result, (-1, -1), "Помилка для відсортованого масиву")
 
         # Весь масив потрібно відсортувати.
-        input_array = [5, 4, 3, 2, 1]
-        result = FindUnsorted.find_unsorted_subarray(input_array)
-        self.assertEqual(result, (0, 4), "Помилка для масиву, який потрібно відсортувати повністю")
+        input_array = [1,2,4,7,10,11,7,12,6,7,16,18,19]
+        result = Solution.find_unsorted_subarray(input_array)
+        self.assertEqual(result, (3, 9), "Помилка для масиву, який потрібно відсортувати повністю")
 
         # Вхідний масив містить лише один елемент.
         input_array = [42]
-        result = FindUnsorted.find_unsorted_subarray(input_array)
+        result = Solution.find_unsorted_subarray(input_array)
         self.assertEqual(result, (-1, -1), "Помилка для масиву з одним елементом")
 
         # Вхідний масив з двома однаковими елементами.
         input_array = [3, 3]
-        result = FindUnsorted.find_unsorted_subarray(input_array)
+        result = Solution.find_unsorted_subarray(input_array)
         self.assertEqual(result, (-1, -1), "Помилка для масиву з двома однаковими елементами")
-
 
 if __name__ == '__main__':
     unittest.main()
