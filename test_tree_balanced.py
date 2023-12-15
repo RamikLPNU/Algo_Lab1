df4d1011bcdc2aaaf596ceb26c1539bfe5b42562
rami_mi <rysys1015@gmail.com>
2023-12-15 16:06:59 +0200

    1

--- test_tree_balanced.py
+++ test_tree_balanced.py
@@ -3,20 +3,31 @@ from tree_balanced import *
 
 class TestIsBalanced(unittest.TestCase):
     def test_balanced_tree(self):
-        root1 = BinaryTree(1)
-        root1.left = BinaryTree(2)
-        root1.right = BinaryTree(3)
-        root1.left.left = BinaryTree(4)
-        root1.left.right = BinaryTree(5)
-        self.assertTrue(is_tree_balanced(root1))
+        root1 = BinaryTree(50)
+        root1.left = BinaryTree(17)
+        root1.left.left = BinaryTree(9)
+        root1.left.left.left = BinaryTree(14)
+        root1.left.left.left.left = BinaryTree(12)
+        root1.left.right = BinaryTree(23)
+        root1.left.right.left = BinaryTree(23)
+        root1.right = BinaryTree(76)
+        root1.right.right = BinaryTree(54)
+        root1.right.right.right = BinaryTree(72)
+        root1.right.right.right.right = BinaryTree(67)
+        self.assertFalse(is_tree_balanced(root1))
 
     def test_unbalanced_tree(self):
-        root2 = BinaryTree(1)
-        root2.left = BinaryTree(2)
-        root2.right = BinaryTree(3)
-        root2.right.right = BinaryTree(4)
-        root2.right.right.right = BinaryTree(5)
-        self.assertFalse(is_tree_balanced(root2))
+        root2 = BinaryTree(50)
+        root2.left = BinaryTree(17)
+        root2.right = BinaryTree(72)
+        root2.right.right = BinaryTree(76)
+        root2.right.left = BinaryTree(54)
+        root2.right.left.right = BinaryTree(67)
+        root2.left.left = BinaryTree(12)
+        root2.left.left.left = BinaryTree(9)
+        root2.left.right = BinaryTree(23)
+        root2.left.right.right = BinaryTree(19)
+        self.assertTrue(is_tree_balanced(root2))
 
     def test_slightly_unbalanced_tree(self):
         root3 = BinaryTree(1)
